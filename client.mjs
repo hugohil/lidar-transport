2f194e89cd6b6757f675102d5a851ff540dbbd20
hugo <hilairehugo@gmail.com>
2022-03-04 18:35:50 +0100

    update sent data

--- client.mjs
+++ client.mjs
@@ -12,8 +12,35 @@ const socket = io(`ws://${argv.address || 'localhost'}:${argv.port || 3000}`);
 
 const re = /S;(.+)E;/gmi;
 
+// convert from polar space to cartesian space
+function p2c (r, theta) {
+  return {
+    x: r * Math.cos(theta),
+    y: r * Math.sin(theta)
+  }
+};
+
 const tail = new Tail(fifofile);
 tail.on('line', (data) => {
   const res = data.match(re);
-  res && socket.emit('data', res[0]);
+
+  if (res) {
+    const datas = res[0].match(/\-?\d+\.\d+/gmi);
+
+    const angles = datas?.filter((d, i) => (i % 2 === 0));
+    const distances = datas?.filter((d, i) => (i % 2 === 1));
+    const lidar = angles?.map((angle, i) => {
+      const a = Number(angle);
+      const d = Number(distances[i] ?? 0);
+      const { x, y } = p2c(d, a);
+      return {
+        x: Number(x.toFixed(2)),
+  y: Number(y.toFixed(2)),
+  a: Number(a.toFixed(2)),
+  d: Number(d.toFixed(2))
+      }
+    }) ?? [];
+
+    socket.emit('data', lidar);
+  }
 });
