759d1fbc0cf01fd58839738f8d00ff6bb6825dc9
hugohil <hilairehugo@gmail.com>
2022-03-23 14:13:13 +0000

    fix last commit

--- client.mjs
+++ client.mjs
@@ -27,7 +27,7 @@ function d2r (deg) {
 const tail = new Tail(fifofile);
 
 tail.on('error', err => { throw(err) });
-tail.on('line', line => {
+tail.on('line', data => {
   const res = data.match(re);
 
   if (res) {
@@ -50,15 +50,9 @@ tail.on('line', line => {
     socket.emit('data', lidar);
   }
 });
-tail.on('ready', fd => console.log('All line are belong to us') );
-tail.on('eof', pos => console.log('Catched up to the last line') );
-tail.on('skip', pos => console.log('file suddenly got replaced with a large file') );
-tail.on('secondary', filename => console.log(`myfile.log is missing. Tailing ${filename} instead`) );
-tail.on('restart', reason => {
-  if( reason == 'PRIMEFOUND' ) console.log('Now we can finally start tailing. File has appeared');
-  if( reason == 'NEWPRIME' ) console.log('We will switch over to the new file now');
-  if( reason == 'TRUNCATE' ) console.log('The file got smaller. I will go up and continue');
-  if( reason == 'CATCHUP' ) console.log('We found a start in an earlier file and are now moving to the next one in the list');
-});
+tail.on('ready', fd => console.log('ready') );
+tail.on('skip', pos => console.log('skipping') );
+tail.on('secondary', filename => console.log(`file is missing. Tailing ${filename} instead.`) );
+tail.on('restart', console.log);
 
 tail.start();
